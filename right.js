5229aff634351de42d0cc9fa5e41894a5502da4a
ysalbel <ysabelliu276@gmail.com>
2016-11-09 13:12:51 -0800

    Add files via upload

--- right.js
+++ right.js
@@ -1,184 +1,271 @@
 var canvas;
 var ctx;
-
+var x = 200;
+var y = 300;
 // order : opposite, side, celling, shadow, door
-function drawGray1() {
-    
+function gray1() {
     "use strict";
     ctx.beginPath();
     ctx.moveTo(604, 136);
     ctx.lineTo(604, 162);
-    
-    ctx.moveTo(604, 162);
     ctx.lineTo(848, 156);
-    
-    ctx.moveTo(848, 156);
     ctx.lineTo(848, 82);
-    
-    ctx.moveTo(848, 82);
-    ctx.lineTo(604, 136);
     ctx.closePath();
     //style
-    
-    ctx.lineWidth = 2;
-    ctx.fillStyle = 'gray';
+    ctx.fillStyle = "rgb(84, 73, 90)";
     ctx.fill();
+    ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
 }
 
-function drawWall1() {
+function wall1() {
     "use strict";
     ctx.beginPath();
     ctx.moveTo(604, 162);
     ctx.lineTo(604, 376);
-    
-    ctx.moveTo(604, 376);
     ctx.lineTo(848, 392);
-    
-    
-    ctx.moveTo(848, 392);
     ctx.lineTo(848, 156);
     ctx.closePath();
     // style
+    ctx.fillStyle = "rgb(253, 187, 108)";
+    ctx.fill();
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
 }
 
-function drawGray2(){
+function gray2(){
     "use strict";
     ctx.beginPath();
     ctx.moveTo(848, 156);
     ctx.lineTo(1073, 173);
-    
-    ctx.moveTo(1073, 173);
     ctx.lineTo(1073, 116);
-    
-    ctx.moveTo(1073, 116);
     ctx.lineTo(848, 82);
-    
-    
     ctx.closePath();
     // style
+    ctx.fillStyle = "rgb(84, 73, 90)";
+    ctx.fill();
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
 }
 
-function drawWall2(){
+function wall2(){
     "use strict";
     ctx.beginPath();
-    ctx.moveTo(848, 392);
-    ctx.lineTo(1073, 350);
-    
-    ctx.moveTo(1073, 350);
+    ctx.moveTo(848, 156);
     ctx.lineTo(1073, 173);
+    ctx.lineTo(1073, 350);
+    ctx.lineTo(848, 392);
     ctx.closePath();
     // style
+    ctx.fillStyle = "rgb(253, 187, 108)";
+    ctx.fill();
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
 }
 
 
-function drawCell1(){
+function cell1(){
     "use strict";
     ctx.beginPath();
     ctx.moveTo(589, 113);
     ctx.lineTo(588, 141);
-    
-    ctx.moveTo(588, 141);
     ctx.lineTo(880, 73);
-    
-    ctx.moveTo(880, 73);
     ctx.lineTo(880, 38);
-    
+    ctx.closePath();
+    // style
+    ctx.fillStyle = "rgb(238, 190, 144)";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+}
+
+function cell2(){
+    "use strict";
+    ctx.beginPath();
     ctx.moveTo(880, 38);
-    ctx.lineTo(589, 113);
-    
+    ctx.lineTo(880,73);
+    ctx.lineTo(1124, 114);
+    ctx.lineTo(1124, 86);
     ctx.closePath();
     // style
+    ctx.fillStyle = "rgb(238, 190, 144)";
+    ctx.fill();
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
 }
 
-function drawCell2(){
+function shadow(){
     "use strict";
     ctx.beginPath();
     ctx.moveTo(880, 73);
-    ctx.lineTo( 1124, 114);
-    
-    ctx.moveTo(1124, 114);
-    ctx.lineTo(1124, 86);
-    
-    ctx.moveTo(1124, 86);
-    ctx.lineTo(880, 38);
-
+    ctx.lineTo(848, 82);
+    ctx.lineTo(1073, 116);
+    ctx.lineTo(1110, 114);
     ctx.closePath();
     // style
+    ctx.fillStyle = "rgb(85, 48, 29)";
+    ctx.fill();
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
 }
 
-function drawShadow(){
+function wall3(){
     "use strict";
     ctx.beginPath();
-    ctx.moveTo(1073, 122);
-    ctx.lineTo(1124, 114);
-    ctx.closePath();
-    // style
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'black';
-    ctx.stroke();
-}
-
-function drawWall3(){
-    "use strict";
-    ctx.beginPath();
-    ctx.moveTo(592, 141);
-    ctx.lineTo(588, 141);
-    
-    ctx.moveTo(588, 141);
-    ctx.lineTo(553, 141);
-    
-    ctx.moveTo(553, 141);
-    ctx.lineTo(536, 138);
-    
     ctx.moveTo(536, 138);
     ctx.lineTo(536, 373);
-    
-    ctx.moveTo(536, 373);
-    ctx.lineTo(604, 376);
+    ctx.lineTo(602, 375);
+    ctx.lineTo(602, 137);
+    ctx.lineTo(588, 141);
+    ctx.lineTo(553, 141);
     ctx.closePath();
     //style
+    ctx.fillStyle = "rgb(131, 85, 62)";
+    ctx.fill();
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
 }
 
-function drawDoor(){
+function door(){
+    "use strict";
+    ctx.beginPath();
     ctx.rect(550, 180, 40, 170);
-    ctx.fillStyle = 'blue';
+    ctx.closePath();
+    ctx.fillStyle = "rgb(70, 102, 179)";
     ctx.fill();
+    ctx.lineWidth = 3;
     ctx.strokeStyle = 'black';
     ctx.stroke();
-    
 }
-function draw() {
+
+// left part
+function middle(){
+    "use strict";
+    ctx.beginPath();
+    ctx.moveTo(552, 110);
+    ctx.lineTo(554, 140);
+    ctx.lineTo(588, 140);
+    ctx.lineTo(588, 116);
+    ctx.closePath();
+    // style
+    ctx.fillStyle = "rgb(238, 190, 144)";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+}
+function cellL1(){
+    "use strict";
+    ctx.beginPath();
+    ctx.moveTo(168, 48);
+    ctx.lineTo(169, 73);
+    ctx.lineTo(554, 141);
+    ctx.lineTo(552, 112);
+    ctx.closePath();
+    // style
+    ctx.fillStyle = "rgb(238, 190, 144)";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+}
+    
+function grayL1(){
+    "use strict";
+    ctx.beginPath();
+    ctx.moveTo(226, 84);
+    ctx.lineTo(228, 168);
+    ctx.lineTo(490, 164);
+    ctx.lineTo(492, 130);
+    ctx.closePath();
+    // style
+    ctx.fillStyle = "rgb(84, 73, 90)";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+}
+
+    function wallL1(){
+    "use strict";
+    ctx.beginPath();
+    ctx.moveTo(228,170);
+    ctx.lineTo(228,357);
+    ctx.lineTo(492,373);
+    ctx.lineTo(492, 130);
+    ctx.lineTo(489, 168);
+    ctx.closePath();
+    // style
+    ctx.fillStyle = "rgb(253, 187, 108)";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+    }
+
+    function shadowL(){
+    "use strict";
+    ctx.beginPath();
+    ctx.moveTo(492, 130);
+    ctx.lineTo(492, 374);
+    ctx.lineTo(537, 368);
+    ctx.lineTo(533, 137);
+    ctx.closePath();
+    // style
+    ctx.fillStyle = "lightgray";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+    }
+        
+    function celling(){
+    "use strict";
+    ctx.beginPath();
+    ctx.moveTo(533, 84);
+    ctx.lineTo(168, 48);
+    ctx.lineTo(552, 112);
+    ctx.lineTo(588, 116);
+    ctx.lineTo(640, 101);
+    ctx.closePath();
+    // style
+    ctx.fillStyle = "rgb(251, 217, 184)";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+    }
+    
+    function draw(){
     "use strict";
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    
-    drawGray1();
-    drawWall1();
-    drawGray2();
-    drawWall2();
-    drawCell1();
-    drawCell2();
-    drawShadow();
-    drawWall3();
-    drawDoor();
-}
+    ctx.save();
+    ctx.scale(0.35,0.35); 
+    ctx.translate(x,y);
+    gray1();
+    wall1();
+    gray2();
+    wall2();
+    cell1();
+    cell2();
+    shadow();
+    wall3();
+    door();
+   
+    middle();
+    cellL1();
+    grayL1();
+    wallL1();
+    shadowL();
+    celling();
+    ctx.restore();
+    }
